cdb50eb7db238c85d77c2a19c1104649c7012845
StartAutomating <james.brundage@start-automating.com>
2026-01-20 00:07:12 -0800

    feat: Graphing comparison ( Fixes #1, Fixes #2 )
    Adjusting CSS

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ $buildTimes | ConvertTo-Html -Title BuildTimes > ./times.html
     "<html>"
     "<head>"
     "<title>4kb Markdown Files</title>"
-    "<style>svg { height: 25%; }</style>"
+    "<style>body { height: 100vh; max-width: 100vw; margin:0 } svg { height: 25%; }</style>"
     "</head>"
     "<body>"
     foreach ($buildTime in $buildTimes) {
