bc3e9bfa55b2a045c814984addc6218055fa5655
StartAutomating <james.brundage@start-automating.com>
2026-01-19 22:35:08 -0800

    feat: build.ps1 ( Fixes #3 )

---
+++ build.ps1
@@ -0,0 +1,38 @@
+param()
+
+Push-Location $PSScriptRoot
+
+& {
+    foreach ($file in Get-ChildItem -filter build.with.*.ps1) {
+        $techniqueName = $file.Name -replace '\.build\.with' -replace '\.ps1$'
+        $time = Measure-Command { . $file.FullName } 
+        [PSCustomObject]@{
+            Technique = $techniqueName
+            Time = $time
+        }
+    }
+} | Tee-Object -Variable buildTimes
+
+
+$buildTimes = $buildTimes | Sort-Object Time
+
+foreach ($buildTime in $buildTimes) {
+    $relativeSpeed = $buildTime.Time.TotalMilliseconds / $buildTimes[-1].Time.TotalMilliseconds
+    Add-Member NoteProperty -InputObject $buildTime -Name RelativeSpeed $relativeSpeed -Force
+} 
+
+$buildTimes | ConvertTo-Html -Title BuildTimes > ./times.html
+
+@(
+    "<html>"
+    "<head>"
+    "<title>4kb Markdown Files</title>"
+    "</head>"
+    "<body>"
+    $buildTimes | ConvertTo-Html -Title BuildTimes -Fragment
+    "</body>"
+    "</html>"
+) > ./index.html
+
+
+Pop-Location
