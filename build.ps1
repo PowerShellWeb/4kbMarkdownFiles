fd9450027e4e5ab848f717e3f80ffa0b0bb95db9
StartAutomating <james.brundage@start-automating.com>
2026-01-19 23:45:33 -0800

    feat: Graphing comparison ( Fixes #1, Fixes #2 )

--- build.ps1
+++ build.ps1
@@ -39,8 +39,23 @@ $buildTimes | ConvertTo-Html -Title BuildTimes > ./times.html
     "<html>"
     "<head>"
     "<title>4kb Markdown Files</title>"
+    "<style>svg { height: 25%; }</style>"
     "</head>"
     "<body>"
+    foreach ($buildTime in $buildTimes) {
+        "<details open>"
+            "<summary>$($buildTime.Technique) ($([Math]::Round(
+                $buildTime.Time.TotalSeconds, 2
+            ))s)</summary>"
+            "<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>"
+                "<rect x='0%' width='1%' height='100%'>"
+                    "<animate attributeName='width' from='1%' to='$([Math]::Round($buildTime.relativeSpeed * 100, 2))%' dur='$($buildTime.Time.TotalSeconds)s' fill='freeze' />"
+                "</rect>"
+            "</svg>"
+        "</details>"
+    }
+    
+    "<h3>The Numbers</h3>"
     $buildTimes | ConvertTo-Html -Fragment
     "</body>"
     "</html>"
