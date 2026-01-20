743e05d196cdab36f592fd422178ff922375e512
StartAutomating <james.brundage@start-automating.com>
2026-01-19 22:48:53 -0800

    feat: build.with.powershell.slow.ps1 ( Fixes #6 )

---
+++ build.with.powershell.slow.ps1
@@ -0,0 +1,26 @@
+param(
+[Alias('Input')]
+[string]
+$InputPath = "$psScriptRoot/TestMarkdown",
+
+[string]
+$OutputPath = "$psScriptRoot/PowerShellSlower"
+)
+
+
+foreach ($file in Get-ChildItem -Path $InputPath -File) {
+    
+    $fileHtml = (ConvertFrom-Markdown -LiteralPath $file.FullName).Html
+    
+    $fileName = $file.Name
+    
+    $directoryPath = $OutputPath, ($fileName -replace '\.md$') -join '/'
+
+    if (-not [IO.Directory]::Exists($directoryPath)) {
+        $null = [IO.Directory]::CreateDirectory($directoryPath)
+    }
+
+    $fileOutputPath = $OutputPath, ($fileName -replace '\.md$'), 'index.html' -join '/'
+    
+    [IO.File]::WriteAllText($fileOutputPath, $fileHtml)    
+}
